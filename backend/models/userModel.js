98f8040a49779a654992a1820c26a335ac257e61
Amalascrobits <90767720+Amala06@users.noreply.github.com>
2023-08-05 16:56:55 +0530

    converted suro model to user

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,154 +4,154 @@ const userSchema = mongoose.Schema(
   {
     name: {
       type: String,
-    },
+    default:"",},
     email: {
       type: String,
       // //required: true,
       unique: true,
-    },
+    default:"",},
     phoneNo: {
       type: String,
       // //required: true,
-    },
+    default:"",},
     password: {
       type: String,
       // //required: true,
-    },
+    default:"",},
     gender: {
       type: String,
       // //required: true,
-    }, //p
+    default:"",}, //p
     city: {
       type: String,
       // //required: true,
-    },
+    default:"",},
     dob: {
       type: String,
       // //required: true,
-    },
+    default:"",},
     sexualOrientation: {
       type: String,
       // //required: true,
-    }, //p
+    default:"",}, //p
     country: {
       type: String,
       // //required: true,
-    },
+    default:"",},
     primaryLanguage: {
       type: String,
       //required: true,
-    },
+    default:"",},
     maritalStatus: {
       type: String,
       //required: true,
-    },
+    default:"",},
     age: {
       type: String,
       //required: true,
-    },
+    default:"",},
     
     numberOfChildren: {
       type: String,
       //required: true,
-    },
+    default:"",},
     anyIllness: {
       type: String,
       //required: true,
-    },
+    default:"",},
     pregnancyExperiance: {
       type: String,
       //required: true,
-    },
+    default:"",},
     surrogacyHistory: {
       type: String,
       //required: true,
-    },
+    default:"",},
     periodsHistory: {
       type: String,
       //required: true,
-    },
+    default:"",},
     useOfAlchohol: {
       type: String,
       //required: true,
-    },
+    default:"",},
     expectationFromSurrogacy: {
       type: String,
       //required: true,
-    },
+    default:"",},
     criminalHistory: {
       type: String,
       //required: true,
-    },
+    default:"",},
     availableForAppointment: {
       type: String,
       //required: true,
-    },
+    default:"",},
     infoOfSupporter: {
       type: String,
       //required: true,
-    },
+    default:"",},
     isVaccinated: {
       type: String,
       //required: true,
-    },
+    default:"",},
     isNRI: {
       type: String,
       //required: true,
-    },
+    default:"",},
     weight: {
       type: String,
       //required: true,
-    },
+    default:"",},
     height: {
       type: String,
       //required: true,
-    },
+    default:"",},
     onAnyMedication: {
       type: String,
       //required: true,
-    },
+    default:"",},
     education: {
       type: String,
       //required: true,
-    },
+    default:"",},
     occupation: {
       type: String,
       //required: true,
-    },
+    default:"",},
     deliveryType: {
       type: String,
       //required: true,
-    },
+    default:"",},
     pic: {
       type: String,
       // //required: true,
       default:
         "https://hips.hearstapps.com/rover/profile_photos/67055711-c808-4a4d-811a-e7155a2bce10_1667409691.file",
-    },
+    default:"",},
     participants: [
       {
         targetid: {
           type: String,
-        },
+        default:"",},
         name: {
           type: String,
-        },
+        default:"",},
         pic: {
           type: String,
-        },
+        default:"",},
         time: {
           type: String,
-        },
+        default:"",},
         message: {
           type: String,
-        },
-      },
+        default:"",},
+      default:"",},
     ],
-  },
+  default:"",},
   {
     timestamps: true,
-  }
+  default:"",}
 );
 // Initialize the participants array with an empty array when creating a new user
 userSchema.pre("save", function (next) {
@@ -162,15 +162,15 @@ userSchema.pre("save", function (next) {
 });
 // userSchema.methods.matchPassword = async function (enteredPassword) {
 //   return await bcrypt.compare(enteredPassword, this.password);
-// };
+// default:"",};
 // userSchema.pre("save", async function (next) {
 //   if (!this.odified) {
 //     next();
-//   }
+//   default:"",}
 
 //   const salt = await bcrypt.genSalt(10);
 //   this.password = await bcrypt.hash(this.password, salt);
-// });
+// default:"",});
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
