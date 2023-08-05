2dba91f983595f3b85f7c087fc1f3b7cfaa20abc
Amalascrobits <90767720+Amala06@users.noreply.github.com>
2023-08-05 16:09:02 +0530

    converted suro model to user

--- backend/controller/surroController.js
+++ backend/controller/surroController.js
@@ -1,5 +1,5 @@
 const asyncHandler = require("express-async-handler");
-const Suro = require("../models/surroModel");
+const User = require("../models/userModel");
 const generateToken = require("../config/generateToken");
 
 const registerSurro = asyncHandler(async (req, res) => {
@@ -40,12 +40,12 @@ const registerSurro = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Please Enter all the Fields");
   }
-  const surroExists = await Suro.findOne({ email });
+  const surroExists = await User.findOne({ email });
   if (surroExists) {
     res.status(404);
     throw new Error("User already Exists");
   }
-  const suro = await Suro.create({
+  const user = await User.create({
     name,
     email,
     password,
@@ -77,39 +77,39 @@ const registerSurro = asyncHandler(async (req, res) => {
     deliveryType,
   });
 
-  if (suro) {
+  if (user) {
     // res.send("Working");
     res.status(201).json({
-      _id: suro._id,
-      name: suro.name,
-      email: suro.email,
-      pic: suro.pic,
-      phoneNo: suro.phoneNo,
-      city: suro.city,
-      dob: suro.dob,
-      country: suro.country,
-      primaryLanguage: suro.primaryLanguage,
-      maritalStatus: suro.maritalStatus,
-      age: suro.age,
-      numberOfChildren: suro.numberOfChildren,
-      anyIllness: suro.anyIllness,
-      pregnancyExperiance: suro.pregnancyExperiance,
-      surrogacyHistory: suro.surrogacyHistory,
-      periodsHistory: suro.periodsHistory,
-      useOfAlchohol: suro.useOfAlchohol,
-      expectationFromSurrogacy: suro.expectationFromSurrogacy,
-      criminalHistory: suro.criminalHistory,
-      availableForAppointment: suro.availableForAppointment,
-      infoOfSupporter: suro.infoOfSupporter,
-      isVaccinated: suro.isVaccinated,
-      price: suro.price,
-      weight: suro.weight,
-      height: suro.height,
-      onAnyMedication: suro.onAnyMedication,
-      education: suro.education,
-      occupation: suro.occupation,
-      deliveryType: suro.deliveryType,
-      token: generateToken(suro._id),
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      phoneNo: user.phoneNo,
+      city: user.city,
+      dob: user.dob,
+      country: user.country,
+      primaryLanguage: user.primaryLanguage,
+      maritalStatus: user.maritalStatus,
+      age: user.age,
+      numberOfChildren: user.numberOfChildren,
+      anyIllness: user.anyIllness,
+      pregnancyExperiance: user.pregnancyExperiance,
+      surrogacyHistory: user.surrogacyHistory,
+      periodsHistory: user.periodsHistory,
+      useOfAlchohol: user.useOfAlchohol,
+      expectationFromSurrogacy: user.expectationFromSurrogacy,
+      criminalHistory: user.criminalHistory,
+      availableForAppointment: user.availableForAppointment,
+      infoOfSupporter: user.infoOfSupporter,
+      isVaccinated: user.isVaccinated,
+      price: user.price,
+      weight: user.weight,
+      height: user.height,
+      onAnyMedication: user.onAnyMedication,
+      education: user.education,
+      occupation: user.occupation,
+      deliveryType: user.deliveryType,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -120,40 +120,40 @@ const registerSurro = asyncHandler(async (req, res) => {
 const authSurro = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
-  const suro = await Suro.findOne({ email });
+  const user = await User.findOne({ email });
 
-  if (suro && password) {
+  if (user && password) {
     res.json({
-      _id: suro._id,
-      name: suro.name,
-      email: suro.email,
-      pic: suro.pic,
-      phoneNo: suro.phoneNo,
-      city: suro.city,
-      dob: suro.dob,
-      country: suro.country,
-      primaryLanguage: suro.primaryLanguage,
-      maritalStatus: suro.maritalStatus,
-      age: suro.age,
-      numberOfChildren: suro.numberOfChildren,
-      anyIllness: suro.anyIllness,
-      pregnancyExperiance: suro.pregnancyExperiance,
-      surrogacyHistory: suro.surrogacyHistory,
-      periodsHistory: suro.periodsHistory,
-      useOfAlchohol: suro.useOfAlchohol,
-      expectationFromSurrogacy: suro.expectationFromSurrogacy,
-      criminalHistory: suro.criminalHistory,
-      availableForAppointment: suro.availableForAppointment,
-      infoOfSupporter: suro.infoOfSupporter,
-      isVaccinated: suro.isVaccinated,
-      price: suro.price,
-      weight: suro.weight,
-      height: suro.height,
-      onAnyMedication: suro.onAnyMedication,
-      education: suro.education,
-      occupation: suro.occupation,
-      deliveryType: suro.deliveryType,
-      token: generateToken(suro._id),
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      phoneNo: user.phoneNo,
+      city: user.city,
+      dob: user.dob,
+      country: user.country,
+      primaryLanguage: user.primaryLanguage,
+      maritalStatus: user.maritalStatus,
+      age: user.age,
+      numberOfChildren: user.numberOfChildren,
+      anyIllness: user.anyIllness,
+      pregnancyExperiance: user.pregnancyExperiance,
+      surrogacyHistory: user.surrogacyHistory,
+      periodsHistory: user.periodsHistory,
+      useOfAlchohol: user.useOfAlchohol,
+      expectationFromSurrogacy: user.expectationFromSurrogacy,
+      criminalHistory: user.criminalHistory,
+      availableForAppointment: user.availableForAppointment,
+      infoOfSupporter: user.infoOfSupporter,
+      isVaccinated: user.isVaccinated,
+      price: user.price,
+      weight: user.weight,
+      height: user.height,
+      onAnyMedication: user.onAnyMedication,
+      education: user.education,
+      occupation: user.occupation,
+      deliveryType: user.deliveryType,
+      token: generateToken(user._id),
     });
   } else {
     res.status(401);
@@ -165,7 +165,7 @@ const allsurrogate = asyncHandler(async (req, res) => {
   // const users = await User.find();
   // res.send(users);
   try {
-    const MyData = await Suro.find();
+    const MyData = await User.find();
     // console.log(MyData); // add this line to log MyData to the console
 
     res.status(200).json(MyData);
@@ -174,29 +174,89 @@ const allsurrogate = asyncHandler(async (req, res) => {
     res.status(500).json({ message: "Could not fetch the data" });
   }
 });
+// const country = asyncHandler(async (req, res) => {
+//   // const users = await User.find();
+//   // res.send(users);
+//   const country = req.params.country;
+//   try {
+//     const MyData = await User.find({ country });
+//     // console.log(MyData); // add this line to log MyData to the console
+// //  {MyData._id,
+// //         MyData.name,
+// //         MyData.email,
+// //         MyData.age,
+// //         MyData.anyIllness,
+// //         MyData.useOfAlchohol,
+// //         MyData.periodsHistory,
+// //         MyData.weight,
+// //         MyData.height,
+// //         MyData.maritalStatus,
+// //         MyData.pregnancyExperiance}
+//     res
+//       .status(200)
+//       .json(
+//         {
+        
+//         email,
+//         age,
+//         anyIllness,
+//         useOfAlchohol,
+//         periodsHistory,
+//         weighheight,
+//         maritalStatus,
+//         pregnancyExperiance}
+//       );
+//   } catch (error) {
+//     console.error(error); // add this line to log any errors to the console
+//     res.status(500).json({ message: "Could not fetch the data" });
+//   }
+// });
 const country = asyncHandler(async (req, res) => {
-  // const users = await User.find();
-  // res.send(users);
   const country = req.params.country;
   try {
-    const MyData = await Suro.find({ country });
-    // console.log(MyData); // add this line to log MyData to the console
+    const MyData = await User.find({ country });
+   if (MyData.length === 0) {
+     res
+       .status(404)
+       .json({ message: "Data not found for the specified country" });
+   } 
+    else{
+      // Extract only the desired fields from MyData
+        const specificResponse = MyData.map((data) => ({
+          id:data._id,
+          name:data.name,
+          email: data.email,
+          age: data.age,
+          anyIllness: data.anyIllness,
+          useOfAlchohol: data.useOfAlchohol,
+          periodsHistory: data.periodsHistory,
+          weight: data.weight ,
+          height:data.height,
+          maritalStatus: data.maritalStatus,
+          pregnancyExperiance: data.pregnancyExperiance,
+        }));
 
-    res.status(200).json(MyData);
+      res.status(200).json(specificResponse);
+    // } else {
+    //   res
+    //     .status(404)
+    //     .json({ message: "Data not found for the specified country" });
+    }
   } catch (error) {
-    console.error(error); // add this line to log any errors to the console
+    console.error(error);
     res.status(500).json({ message: "Could not fetch the data" });
   }
 });
+
 const age = asyncHandler(async (req, res) => {
   // const users = await User.find();
   // res.send(users);
   const age = req.params.country;
   try {
-    const MyData = await Suro.find({ age });
+    const MyData = await User.find({ age });
     // console.log(MyData); // add this line to log MyData to the console
 
-    res.status(200).json(MyData);
+    res.status(200).json(MyData._id,MyData.name,MyData.email,MyData.age,MyData.anyIllness,MyData.useOfAlchohol,MyData.periodsHistory,MyData.weight,MyData.height,MyData.maritalStatus,MyData.pregnancyExperiance);
   } catch (error) {
     console.error(error); // add this line to log any errors to the console
     res.status(500).json({ message: "Could not fetch the data" });
@@ -207,7 +267,7 @@ const maritalStatus = asyncHandler(async (req, res) => {
   // res.send(users);
   const mar = req.params.maritalStatus;
   try {
-    const MyData = await Suro.find({ mar });
+    const MyData = await User.find({ mar });
     // console.log(MyData); // add this line to log MyData to the console
 
     res.status(200).json(MyData);
@@ -221,7 +281,7 @@ const primarylang = asyncHandler(async (req, res) => {
   // res.send(users);
   const primaryLanguage = req.params.primaryLanguage;
   try {
-    const MyData = await Suro.find({ primaryLanguage });
+    const MyData = await User.find({ primaryLanguage });
     // console.log(MyData); // add this line to log MyData to the console
 
     res.status(200).json(MyData);
@@ -236,7 +296,7 @@ const price = asyncHandler(async (req, res) => {
   // res.send(users);
   const price = req.params.price;
   try {
-    const MyData = await Suro.find({ price });
+    const MyData = await User.find({ price });
     // console.log(MyData); // add this line to log MyData to the console
 
     res.status(200).json(MyData);
@@ -246,7 +306,7 @@ const price = asyncHandler(async (req, res) => {
   }
 });
 
-const suroSorted = asyncHandler(async (req, res) => {
+const UserSorted = asyncHandler(async (req, res) => {
   const keyword = req.query.search
     ? {
         $or: [
@@ -255,10 +315,10 @@ const suroSorted = asyncHandler(async (req, res) => {
         ],
       }
     : {};
-  const surrogacy = await Suro.find(keyword);
+  const surrogacy = await User.find(keyword);
   res.send(surrogacy);
 });
-// const suroSorted = asyncHandler(async (req, res) => {
+// const UserSorted = asyncHandler(async (req, res) => {
 //   const keyword = req.query.search
 //     ? {
 //         $or: [
@@ -268,7 +328,7 @@ const suroSorted = asyncHandler(async (req, res) => {
 //       }
 //     : {};
 
-//   const surrogacy = await Suro.find(keyword);
+//   const surrogacy = await User.find(keyword);
 
 //   res.status(200).json({
 //     keyword: keyword, // Include the keyword object in the response
@@ -279,7 +339,7 @@ const suroSorted = asyncHandler(async (req, res) => {
 module.exports = {
   registerSurro,
   authSurro,
-  suroSorted,
+  UserSorted,
   allsurrogate,
   country,
   age,
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -7,125 +7,125 @@ const userSchema = mongoose.Schema(
     },
     email: {
       type: String,
-      // required: true,
+      // //required: true,
       unique: true,
     },
     phoneNo: {
       type: String,
-      // required: true,
+      // //required: true,
     },
     password: {
       type: String,
-      // required: true,
+      // //required: true,
     },
     gender: {
       type: String,
-      // required: true,
+      // //required: true,
     }, //p
     city: {
       type: String,
-      // required: true,
+      // //required: true,
     },
     dob: {
       type: String,
-      // required: true,
+      // //required: true,
     },
     sexualOrientation: {
       type: String,
-      // required: true,
+      // //required: true,
     }, //p
     country: {
       type: String,
-      // required: true,
+      // //required: true,
     },
-    // primaryLanguage: {
-    //   type: String,
-    //   required: true,
-    // },
-    // maritalStatus: {
-    //   type: String,
-    //   required: true,
-    // },
-    // age: {
-    //   type: String,
-    //   required: true,
-    // },
-    //s
-    // numberOfChildren: {
-    //   type: String,
-    //   required: true,
-    // },
-    // anyIllness: {
-    //   type: String,
-    //   required: true,
-    // },
-    // pregnancyExperiance: {
-    //   type: String,
-    //   required: true,
-    // },
-    // surrogacyHistory: {
-    //   type: String,
-    //   required: true,
-    // },
-    // periodsHistory: {
-    //   type: String,
-    //   required: true,
-    // },
-    // useOfAlchohol: {
-    //   type: String,
-    //   required: true,
-    // },
-    // expectationFromSurrogacy: {
-    //   type: String,
-    //   required: true,
-    // },
-    // criminalHistory: {
-    //   type: String,
-    //   required: true,
-    // },
-    // availableForAppointment: {
-    //   type: String,
-    //   required: true,
-    // },
-    // infoOfSupporter: {
-    //   type: String,
-    //   required: true,
-    // },
-    // isVaccinated: {
-    //   type: String,
-    //   required: true,
-    // },
-    // isNRI: {
-    //   type: String,
-    //   required: true,
-    // },
-    // weight: {
-    //   type: String,
-    //   required: true,
-    // },
-    // height: {
-    //   type: String,
-    //   required: true,
-    // },
-    // onAnyMedication: {
-    //   type: String,
-    //   required: true,
-    // },
-    // education: {
-    //   type: String,
-    //   required: true,
-    // },
-    // occupation: {
-    //   type: String,
-    //   required: true,
-    // },
-    // deliveryType: {
-    //   type: String,
-    //   required: true,
-    // },
+    primaryLanguage: {
+      type: String,
+      //required: true,
+    },
+    maritalStatus: {
+      type: String,
+      //required: true,
+    },
+    age: {
+      type: String,
+      //required: true,
+    },
+    
+    numberOfChildren: {
+      type: String,
+      //required: true,
+    },
+    anyIllness: {
+      type: String,
+      //required: true,
+    },
+    pregnancyExperiance: {
+      type: String,
+      //required: true,
+    },
+    surrogacyHistory: {
+      type: String,
+      //required: true,
+    },
+    periodsHistory: {
+      type: String,
+      //required: true,
+    },
+    useOfAlchohol: {
+      type: String,
+      //required: true,
+    },
+    expectationFromSurrogacy: {
+      type: String,
+      //required: true,
+    },
+    criminalHistory: {
+      type: String,
+      //required: true,
+    },
+    availableForAppointment: {
+      type: String,
+      //required: true,
+    },
+    infoOfSupporter: {
+      type: String,
+      //required: true,
+    },
+    isVaccinated: {
+      type: String,
+      //required: true,
+    },
+    isNRI: {
+      type: String,
+      //required: true,
+    },
+    weight: {
+      type: String,
+      //required: true,
+    },
+    height: {
+      type: String,
+      //required: true,
+    },
+    onAnyMedication: {
+      type: String,
+      //required: true,
+    },
+    education: {
+      type: String,
+      //required: true,
+    },
+    occupation: {
+      type: String,
+      //required: true,
+    },
+    deliveryType: {
+      type: String,
+      //required: true,
+    },
     pic: {
       type: String,
-      // required: true,
+      // //required: true,
       default:
         "https://hips.hearstapps.com/rover/profile_photos/67055711-c808-4a4d-811a-e7155a2bce10_1667409691.file",
     },
